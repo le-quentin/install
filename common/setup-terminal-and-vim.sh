3135483db8b79abfcd19da33e4636a49537b4402
Quentin Bonnet <quentin.bonnet.work@gmail.com>
2022-10-14 17:47:30 +0200

    Install tmux-powerline in home dir

--- common/setup-terminal-and-vim.sh
+++ common/setup-terminal-and-vim.sh
@@ -14,3 +14,4 @@ gvim -v -u ~/.vimrc.ide +'PlugInstall --sync' +qa
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" "--unattended"
 mv ~/.zshrc.pre-oh-my-zsh ~/.zshrc
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone https://github.com/erikw/tmux-powerline.git $HOME/.tmux-powerline
