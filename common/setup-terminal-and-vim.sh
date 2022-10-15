cc54fe08469cb4e4232554e7005a93a1954c6e80
Quentin Bonnet <quentin.bonnet.work@gmail.com>
2022-10-15 16:36:18 +0200

    Lenient arch install: delete dirs first, for indempotence

--- common/setup-terminal-and-vim.sh
+++ common/setup-terminal-and-vim.sh
@@ -11,7 +11,9 @@ gvim -v +'PlugInstall --sync' +qa
 gvim -v -u ~/.vimrc.ide +'PlugInstall --sync' +qa
 
 # fancy terminal
+rm -rf ~/.oh-my-zsh 2>/dev/null || true
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" "--unattended"
 mv ~/.zshrc.pre-oh-my-zsh ~/.zshrc
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+rm -rf ~/.tmux-powerline 2>/dev/null || true
 git clone https://github.com/erikw/tmux-powerline.git $HOME/.tmux-powerline
