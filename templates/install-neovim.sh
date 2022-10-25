7eec197656a3727d3639a7ed329034da17acf448
Quentin Bonnet <quentin.bonnet.work@gmail.com>
2022-10-25 02:08:08 +0200

    Script to install nvim, not used for now

---
+++ templates/install-neovim.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+#Exit on any command fail
+set -e
+
+#Snippet to parse named args
+for ARGUMENT in "$@"
+do
+   KEY=$(echo $ARGUMENT | cut -f1 -d=)
+
+   KEY_LENGTH=${#KEY}
+   VALUE="${ARGUMENT:$KEY_LENGTH+1}"
+
+   export "$KEY"="$VALUE"
+done
+
+$INSTALL_CMD neovim
+
+# Install vim-plug
+curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+# Install plugins
+nvim -es -u init.vim -i NONE -c "PlugInstall" -c "qa"
