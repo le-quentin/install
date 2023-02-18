2feaee78fbdf9d2f14996195306be6dfdee6b45a
Quentin Bonnet <quentin.bonnet.work@gmail.com>
2023-02-18 16:56:43 +0100

    Docker setup for cross compilation

--- templates/install-docker.sh
+++ templates/install-docker.sh
@@ -18,3 +18,9 @@ done
 $INSTALL_CMD docker docker-compose
 systemctl enable docker --now
 usermod -aG docker $USER
+
+# Install docker buildx, for cross-platform builds, and qemu-user-static, to enable cross build for various platforms
+# Then do the necessary docker setup
+$INSTALL_CMD docker-buildx qemu-user-static
+docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+
