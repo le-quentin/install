8e018becf9c74469622659134ee9407f407aacf1
Quentin Bonnet <quentin.bonnet.work@gmail.com>
2023-04-18 15:09:55 +0200

    Install SF-Pro fonts

--- common/install-fonts.sh
+++ common/install-fonts.sh
@@ -6,7 +6,7 @@ set -e
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 
 echo "#################################Installing fonts..."
-cp "$DIR"/*.ttf /usr/share/fonts/
-cp "$DIR"/*.otf /usr/share/fonts/
+cp "$DIR"/fonts/*.ttf /usr/share/fonts/
+cp "$DIR"/fonts/*.otf /usr/share/fonts/
 fc-cache -f -v
 
