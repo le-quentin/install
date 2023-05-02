c0bbf3e9730a1912bb599518937d60cfb783b523
Quentin Bonnet <quentin.bonnet.work@gmail.com>
2023-05-03 00:41:41 +0200

    Install vscode-js-debug, for nvim nodejs debug

--- common/setup-terminal-and-vim.sh
+++ common/setup-terminal-and-vim.sh
@@ -10,6 +10,10 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.c
 gvim -v +'PlugInstall --sync' +qa
 gvim -v -u ~/.vimrc.ide +'PlugInstall --sync' +qa
 
+# Install vscode-js-debug, for nodejs debug inside nvim
+# The installation script should have been put in ~/.local/bin/ by chezmoi; and it should be in path
+install-vscode-js-debug
+
 # fancy terminal
 rm -rf ~/.oh-my-zsh 2>/dev/null || true
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" "--unattended"
